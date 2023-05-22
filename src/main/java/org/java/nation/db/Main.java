5567045634ac2e081b076416986fe500a2294a81
francescotrudu <francescotrudu84@gmail.com>
2023-05-22 17:20:54 +0200

    Added milestone 3

--- src/main/java/org/java/nation/db/Main.java
+++ src/main/java/org/java/nation/db/Main.java
@@ -5,6 +5,7 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Scanner;
 
 public class Main {
 
@@ -14,17 +15,28 @@ public class Main {
 		String password = "root";
 		
 		
-				try(Connection con = DriverManager.getConnection(url, user, password)){
+
+		try (Scanner sc = new Scanner(System.in);
+				Connection con = DriverManager.getConnection(url, user, password)){
 		    
 			String sql = " SELECT c.name, c.country_id, r.name, c2.name"
 					+ " FROM countries c "
 					+ " JOIN regions r ON c.region_id = r.region_id "
 					+ " JOIN continents c2 ON r.continent_id = c.region_id "
+					+ " WHERE c.name LIKE ? "
 					+ " ORDER  BY c.name";
 			
-			System.out.print("Lista delle nazioni ordinate per nome: \n");
+//			MILESTONE 2
+//			System.out.print("Lista delle nazioni ordinate per nome: \n");
+			
+//			MILESTONE 3
+			System.out.print("Inserisci testo per filtrare la ricerca: ");
+			String inputSearch = sc.nextLine();
+			
 			
 			try (PreparedStatement ps = con.prepareStatement(sql)) {
+				 ps.setString(1, "%" + inputSearch + "%");
+				
 				try (ResultSet rs = ps.executeQuery()) {
 					
 					while(rs.next()) {
